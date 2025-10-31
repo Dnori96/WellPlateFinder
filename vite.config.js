d3c0dd310241eee80c27291360da9fae495bbd1a
Diego Noriega <Diego.ale036@gmail.com>
2025-10-31 16:27:05 -0500

    feat: update Vite configuration to include path aliases for images 🤩

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +14,10 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/__tests__',
+  },
+  resolve: {
+    alias: {
+      '@/images': path.resolve(__dirname, 'src/assets/images/'),
+    },
   },
 });
