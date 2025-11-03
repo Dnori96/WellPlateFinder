5501dead7acfdcc6c8f5e082154667023561a29d
Diego Noriega <Diego.ale036@gmail.com>
2025-11-03 16:09:23 -0600

    feat: implement recipe filtering and search functionality in Recipes page 🔍

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,3 +1,76 @@
+import initialRecipes from '@/lib/mock/data.json';
+import Plate from '@/components/Plate';
+import { useState } from 'react';
+
 export default function Recipes() {
-  return <div className="flex justify-center items-center h-dvh">RECIPES Page</div>;
+  const [plates] = useState(initialRecipes);
+  const [filters, setFilters] = useState({
+    prepTime: 'all',
+    cookTime: 'all',
+  });
+  const [inputQuery, setinputQuery] = useState('');
+
+  const handleInputSearch = (perPlate) => {
+    if (!inputQuery) return perPlate;
+    const searchPlate = perPlate.title.toLowerCase();
+    const inputSearch = inputQuery.toLowerCase();
+    return searchPlate.includes(inputSearch);
+  };
+
+  const filterProducts = (plates) => {
+    return plates.filter((p) => {
+      return (
+        handleInputSearch(p) &&
+        (filters.prepTime == 'all' || p.prepMinutes == filters.prepTime) &&
+        (filters.prepTime == 'all' || p.cookMinutes == filters.cookTime)
+      );
+    });
+  };
+
+  const filteredProducts = filterProducts(plates);
+
+  return (
+    <main className="mx-auto flex flex-col items-center max-w-[1200px] w-full">
+      <section className="text-center flex flex-col gap-3 p-[80px_0_64px_0]">
+        <h1 className="text-5xl/[57px] tracking-[-2px] text-primary-Neutral-900 font-extrabold">
+          Explore our simple, healthy recipes
+        </h1>
+        <p className="font-Secundary text-xl/[30px] tracking-[-0.4px] font-normal max-w-[724px]">
+          Discover eight quick, whole-food dishes that fit real-life schedules and taste amazing. Use the search bar to
+          find a recipe by name or ingredient, or simply scroll the list and let something delicious catch your eye.
+        </p>
+      </section>
+      <section className="w-full">
+        <search className="flex items-center justify-between w-full">
+          <div className="flex">
+            <menu>prep time</menu>
+            <menu>max cook time</menu>
+          </div>
+          <input
+            type="text"
+            placeholder="Search by name or ingredient…"
+            onChange={(e) => setinputQuery(e.target.value)}
+          />
+        </search>
+      </section>
+      <section className="flex content-start items-start self-stretch gap-9 flex-wrap">
+        {filteredProducts.map((p) => (
+          <Plate
+            key={p.id}
+            images={{
+              imgLarge: p.image.large,
+              imgSmall: p.image.small,
+            }}
+            desc={{
+              title: p.title,
+              text: p.overview,
+              servings: p.servings,
+              prepTime: p.prepMinutes,
+              cookTime: p.cookMinutes,
+            }}
+          />
+        ))}
+      </section>
+    </main>
+  );
 }
