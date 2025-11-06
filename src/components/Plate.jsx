6de0e16f27375c24beb89c3d84a3daf6aee9913b
Diego Noriega <Diego.ale036@gmail.com>
2025-11-05 19:57:28 -0600

    feat: enhance Plate component layout and styling for improved presentation

--- src/components/Plate.jsx
+++ src/components/Plate.jsx
@@ -1,23 +1,41 @@
 import { Link } from 'react-router-dom';
+import servings from '@/images/icons/icon-servings.svg';
+import prep from '@/images/icons/icon-prep-time.svg';
+import cook from '@/images/icons/icon-cook-time.svg';
 
 export default function Plate({ images, desc }) {
   return (
-    <article className="max-w-[376px]">
-      <picture className="max-md:flex max-md:flex-col max-md:items-center">
+    <article className="max-w-[376px] p-2 flex flex-col gap-4 rounded-[10px] border border-primary-Neutral-300 bg-primary-Neutral-0 shadow-[0_8px_16px_-9px_rgba(22,58,52,0.16)]">
+      <picture className="">
         <source media="(min-width: 768px)" srcSet={images.imgLarge} />
-        <img src={images.imgSmall} alt="Large photo of a woman cooking" />
+        <img src={images.imgSmall} alt="Large photo of a woman cooking" className="shrink-0 aspect-square rounded-xl" />
       </picture>
-      <div>
-        <h3>{desc.title}</h3>
-        <p>{desc.text}</p>
+      <div className="flex flex-col gap-2.5 self-stretch p-[0_8px]">
+        <h3 className="whitespace-nowrap text-ellipsis overflow-hidden w-full text-primary-Neutral-900 text-xl/[28px] font-bold tracking-[-0.5px]">
+          {desc.title}
+        </h3>
+        <p className="text-primary-Neitral-800 self-stretch text-base/[24px] tracking-[-0.3px] font-medium">
+          {desc.text}
+        </p>
       </div>
-      <div>
-        <span>{`Servings: ${desc.servings}`}</span>
-        <span>{`Prep: ${desc.prepTime} ${desc.prepTime >= 10 ? 'mins' : 'min'}`}</span>
-        <span>{`Cook: ${desc.cookTime} ${desc.cookTime >= 10 ? 'mins' : 'min'}`}</span>
+      <div className="flex flex-wrap self-stretch items-center content-center p-[0_8px] gap-[8px_16px] cursor-default">
+        <span className="flex text-base/[24px] tracking-[-0.3px] font-medium">
+          <img src={servings} alt="Total Servings of the recipe" className="size-5" />
+          {`Servings: ${desc.servings}`}
+        </span>
+        <span className="flex text-base/[24px] tracking-[-0.3px] font-medium">
+          <img src={prep} alt="Minutes to prepare the recipe" className="size-5" />
+          {`Prep: ${desc.prepTime} ${desc.prepTime >= 1 ? 'mins' : 'min'}`}
+        </span>
+        <span className="flex text-base/[24px] tracking-[-0.3px] font-medium">
+          <img src={cook} alt="Minutes to cook the recipe" className="size-5" />
+          {`Cook: ${desc.cookTime} ${desc.cookTime >= 1 ? 'mins' : 'min'}`}
+        </span>
       </div>
       <Link>
-        <button className="bg-primary-Neutral-900 text-primary-Neutral-0">View Recipe</button>
+        <button className="text-base/[24px] tracking-[-0.3px] cursor-pointer font-bold hover:bg-primary-Neitral-800 bg-primary-Neutral-900 text-primary-Neutral-0 w-full p-[12px_32px] rounded-full">
+          View Recipe
+        </button>
       </Link>
     </article>
   );
