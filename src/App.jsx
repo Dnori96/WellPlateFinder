ca5442719c7833ad796a29b8018dcb4a92c9c345
Diego Noriega <Diego.ale036@gmail.com>
2025-11-07 12:52:25 -0600

    Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Footer from '@/components/Footer';
 import { Outlet } from 'react-router-dom';
 import PageProvider from '@/context/pageContext';
 import RecipesProvider from '@/context/recipesContext';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
           <Header />
           <Outlet />
           <Footer />
+          <Analytics />
         </div>
       </PageProvider>
     </RecipesProvider>
